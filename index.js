dd90218673ef922a221b398beb4b6378aac1916f
archi-od <fanat12341233@gmail.com>
2022-03-21 20:07:00 +0200

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const TelegramApi = require('node-telegram-bot-api')
+const TelegramApi = require('node-telegram-bot-api');
 const {gameOptions, againOptions} = require('./options');
-const token = '5278587444:AAHesmGYwMsyjUC-etB8LW_MC43il-R9Wu4'
+const token = '5278587444:AAHesmGYwMsyjUC-etB8LW_MC43il-R9Wu4';
 
-const bot = new TelegramApi(token, {polling: true})
+const bot = new TelegramApi(token, {polling: true});
 
-const chats = {}
+const chats = {};
 
 const startGame = async (chatId) => {
     await bot.sendMessage(chatId, 'Я загадаю цифру от 0 до 9, а ты попробуй угадай, если не угадаешь я оформлю на тебя мой кредит! \nПопытка не пытка, но помни как Юлий проиграл два полцарства', gameOptions);
@@ -16,7 +16,7 @@ const start = () => {
         {command: '/start', description: 'Приветствие'},
         {command: '/info', description: 'Твои персональные данные уже у меня'},
         {command: '/game', description: 'Какая-то игра непонятная'},
-    ])
+    ]);
 
     bot.on('message', async msg => {
         const text = msg.text;
@@ -31,22 +31,22 @@ const start = () => {
         if (text === '/game') {
             return startGame(chatId);
         }
-        console.log(msg);
         return bot.sendMessage(chatId, 'Ты чё несешь??? Нормальную команду выбери!!!');
     });
 
-    bot.on('callback_query', msg => {
+    bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
         if (data === '/again') {
             return startGame(chatId);
         }
         if (Number(data) === chats[chatId]) {
-            return bot.sendMessage(chatId, `Жулик ты удагал цифру ${chats[chatId]}`, againOptions);
+            await bot.sendMessage(chatId, `Жулик ты удагал цифру ${chats[chatId]}`);
+            return bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/32c/096/32c0964b-622b-3372-830a-35b839713818/3.webp', againOptions);
         } else {
             return bot.sendMessage(chatId, `Жаль но вы проиграли, встерчаемся завтра в банке бери с собой паспорт я загадал цифру ${chats[chatId]}`, againOptions);
         }
-    })
+    });
 }
 
 start();
